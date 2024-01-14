53a3ddf9882900e48efa88b4022e1c83c75da843
nwin19 <nandarwinbuma@gmail.com>
2024-01-14 23:09:58 +0630

    showing data from params

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,14 @@
-export default function TopicShowPage() {
-  return <div>Topic Show</div>;
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = params;
+  return (
+    <div>
+      <h1 className="font-bold text-2xl mb-2">{slug}</h1>
+    </div>
+  );
 }
